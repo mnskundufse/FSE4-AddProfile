252cf3bc58b07fd1dd55d8bb46b162ce64993f14
mnskundu <mnskundu.fse@gmail.com>
2022-05-31 10:55:29 +0530

    Initial Checkin

---
+++ Engineer.AddProfileService/Engineer.AddProfileService.Test/AddProductControllerTest.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using Xunit;
+using Moq;
+using Seller.AddProductService.Business.Contracts;
+using Microsoft.Extensions.Logging;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Seller.AddProductService.Controllers;
+using Seller.AddProductService.Model;
+using Seller.AddProductService.Kafka;
+using System.Threading;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Seller.AddProductService.Test
+{
+    public class AddProductControllerTest
+    {
+        readonly Mock<IAddProductBusiness> _mockProductBusiness = new Mock<IAddProductBusiness>();
+        readonly Mock<IProducerWrapper> _mockProducerWrapper = new Mock<IProducerWrapper>();
+        readonly Mock<ILogger<AddProductController>> _mockLogger = new Mock<ILogger<AddProductController>>();
+        readonly Mock<ProducerConfig> _mockProducerConfig = new Mock<ProducerConfig>();
+
+        [Fact]
+        public async Task AddSellerProduct_ValidResponse()
+        {
+            Product request = new Product
+            {
+                ProductName = "xyzabc",
+                ShortDescription = "xyzabc",
+                DetailedDescription = "xyzabc",
+                Category = "Painting",
+                StartingPrice = 10,
+                BidEndDate = DateTime.Now
+            };
+
+            ApiResponse response = new ApiResponse()
+            {
+                Result = 2,
+                Status = new StatusResponse
+                {
+                    IsValid = true,
+                    Status = "SUCCESS",
+                    Message = string.Empty
+                }
+            };
+
+            AddProductController _testObject = new AddProductController(_mockLogger.Object, _mockProducerConfig.Object, _mockProductBusiness.Object, _mockProducerWrapper.Object);
+            ProducerWrapper _producerTestObject = new ProducerWrapper(_mockProducerConfig.Object);
+            Mock<IProducer<string, string>> _mockProducerBuilder = new Mock<IProducer<string, string>>();
+            _mockProducerWrapper.Setup(x => x.WriteMessage(It.IsAny<string>(), It.IsAny<string>()));
+            _mockProductBusiness.Setup(x=>x.AddSellerProductBusiness(It.IsAny<Product>())).Returns(Task.FromResult(response));
+            var result = (ObjectResult) await _testObject.AddSellerProduct(request);
+
+            ApiResponse apiResponse = (ApiResponse)result.Value;
+
+            Assert.NotNull(apiResponse.Result);
+            Assert.Equal(2, (int)apiResponse.Result);
+            Assert.NotNull(apiResponse.Status);
+            Assert.True(apiResponse.Status.IsValid);
+            Assert.Equal("SUCCESS", apiResponse.Status.Status);
+            Assert.Empty(apiResponse.Status.Message);
+        }
+    }
+}
---
+++ Engineer.AddProfileService/Engineer.AddProfileService/Controllers/AddProfileController.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Threading.Tasks;
+using Confluent.Kafka;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+using Newtonsoft.Json;
+using Engineer.AddProfileService.Business.Contracts;
+using Engineer.AddProfileService.Kafka;
+using Engineer.AddProfileService.Model;
+
+namespace Engineer.AddProfileService.Controllers
+{
+    [ApiController]
+    [ApiVersion("1.0")]
+    [Route("skill-tracker/api/v{version:apiVersion}/engineer")]
+    [Produces("application/json")]
+    public class AddProfileController: ControllerBase
+    {
+        private readonly ILogger<AddProfileController> _logger;
+        private readonly ProducerConfig _config;
+        private readonly IAddProfileBusiness _addProfileBC;
+        private readonly IProducerWrapper _producerWrapper;
+
+        public AddProfileController(ILogger<AddProfileController> logger, ProducerConfig config, IAddProfileBusiness addProfileBC, IProducerWrapper producerWrapper)
+        {
+            _addProfileBC = addProfileBC;
+            _producerWrapper = producerWrapper;
+            _logger = logger;
+            _config = config;
+        }
+
+        [MapToApiVersion("1.0")]
+        [Authorize]
+        [HttpPost("add-profile")]
+        public async Task<IActionResult> AddUserProfile(UserProfile userProfile)
+        {
+            ApiResponse response = new ApiResponse();
+            try
+            {
+                bool insertOperationFlag = false;
+                if (ModelState.IsValid)
+                {
+                    userProfile.CreatedDate = DateTime.Now;
+                    userProfile.UpdatedDate = DateTime.Now;
+                    response = await _addProfileBC.AddUserProfileBusiness(userProfile);
+
+                    long result = 0;
+                    long.TryParse(Convert.ToString(response.Result ?? 0), out result);
+                    if (result > 0)
+                    {
+                        userProfile.UserId = result;
+                        PublishEvent(userProfile);
+                        insertOperationFlag = true;
+                    }
+                }
+                else
+                {
+                    response.Status.Message = "Invalid Input";
+                    response.Status.Status = "FAIL";
+                    response.Status.IsValid = false;
+                }
+
+                if (insertOperationFlag)
+                {
+                    _logger.LogInformation("{date} : AddUserProfile of the AddProfileController executed.", DateTime.UtcNow);
+                    return StatusCode(200, response);
+                }
+                else
+                {
+                    _logger.LogInformation("{date} : AddUserProfile of the AddProfileController Failed : Message {message} ", DateTime.UtcNow, response.Status.Message);
+                    return StatusCode(405, response);
+                }
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError(ex, "An unknown error occurred on the AddUserProfile of the AddProfileController.");
+                throw ex;
+            }
+        }
+
+        /// <summary>
+        /// Publish Add UserProfile event to profileforadminaddtopic (KAFKA Code)
+        /// </summary>
+        /// <param name="userProfileForAdmin"></param>
+        private async void PublishEvent(UserProfile userProfileForAdmin)
+        {
+            string serializedUserProfileForAdmin = JsonConvert.SerializeObject(userProfileForAdmin);
+            await _producerWrapper.WriteMessage(serializedUserProfileForAdmin, "profileforadminaddtopic");
+
+            _logger.LogInformation("{date} : PublishEvent of the AddProfileController executed.", DateTime.UtcNow);
+        }
+    }
+}
